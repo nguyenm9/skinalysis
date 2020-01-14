74790993d1b8f073513980e216770e3b0c164b81
nguyenm9 <micnguyen@ucdavis.edu>
2020-01-13 23:32:20 -0800

    Install axios and clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,14 @@
 import React, {Component} from 'react';
-import { Menu, Sidebar, Segment, Button } from 'semantic-ui-react';
-import Home from './components/Home';
-import Nav from './components/Nav';
-import Analyze from './components/Analyze';
 import TopHeader from './components/TopHeader';
 import InnerComponent from './components/InnerComponent';
 
 export default class MainContainer extends Component {
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         visible: false
-    //     }
-    // }
-
-    // toggleMenu = () => {
-    //     this.setState({visible: !this.state.visible});
-    // }
-
     render() {
         return (
             <div>
                 <TopHeader/>
                 <InnerComponent/>
             </div>
-
-            
-
-                // <div className="App" style={{width: '1000px', height: '900px', textAlign: 'center'}}>
-                //     <Sidebar.Pushable>
-                //         <Sidebar
-                //             as={Menu}
-                //             animation='push'
-                //             icon='labeled'
-                //             inverted
-                //             vertical
-                //             visible={this.state.visible}
-                //             width='thin'
-                //         >
-                //             <Nav/>
-                //         </Sidebar>
-                //         <Sidebar.Pusher>
-                //             <Button basic onClick={this.toggleMenu}>Toggle</Button>
-                //             <Segment basic>
-                //                 { this.state.currentView === "home" ? (
-                //                     <Home/>
-                //                 ):(
-                //                     <Analyze/>
-                //                 )}
-                //             </Segment>
-                //         </Sidebar.Pusher>
-                //     </Sidebar.Pushable>
-                // </div>
         )
     }
   }
